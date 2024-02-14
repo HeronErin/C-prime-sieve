360e6a613719c4ae4f5f95def17f24b6ba86aa8d
Mitchell <80652149+HeronErin@users.noreply.github.com>
2024-02-14 18:44:19 +0000

    added copyright notice

--- readmeGen/genGraph.py
+++ readmeGen/genGraph.py
@@ -1,6 +1,20 @@
+#  Copyright (C) 2024  HeronErin
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import matplotlib.pyplot as plt
 import datetime, os
-# datetime_object = datetime.strptime(datetime_str, '%m/%d/%Y %H:%M:%S')
 
 y = {}
 x = {}

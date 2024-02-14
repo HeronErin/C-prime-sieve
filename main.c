bb023819f24c6920b7dbacad8082ef98ba32b3a3
Mitchell <80652149+HeronErin@users.noreply.github.com>
2024-02-14 15:34:56 +0000

    it might be working

--- main.c
+++ main.c
@@ -2,19 +2,67 @@
 #include "root.c"
 #include <stdlib.h>
 
+#define EXTENT 100
 typedef int itype;
+#define sqrt_f fast_sqrt_ui
 
 struct Number{
     itype x;
     struct Number* next;
 };
+void printNumbers(struct Number* itr){
+    printf("---------------\n");
+    while (itr){
+        printf("- %i\n", itr->x);
+        itr=itr->next;
+    }
+    printf("---------------\n");
+}
 
-#define EXTENT 100
-int main(){
+struct Number* makeNumberBuffer(){
+    // Get the memory
     struct Number* numbers = (struct Number*) malloc(sizeof(struct Number) * EXTENT);
+    
+    // Fill the linked list:
+    numbers->x = 2;
+    for (itype x = 3; x < EXTENT+1; x++){
+        numbers[x-2].x = x;
+        numbers[x-3].next = &numbers[x-2];
+    }
+    numbers[EXTENT-2].next = 0;
+    
+    return numbers;
+} 
 
-    // for ()
+
+void _primeEliminationStage(struct Number* itr){
+    itype divisor = itr->x;
+    struct Number* last_possible_prime = itr;
+
+    itr=itr->next;
+    while(itr){
+        if (itr->x % divisor == 0)
+            last_possible_prime->next = itr->next;
+        else
+            last_possible_prime = itr;
+        
+        itr = itr->next;
+    }
+}
+void eleminateNonPrimes(struct Number* list){
+    itype eleminationLimit = sqrt_f(EXTENT)+1;
+    while (list && list->x <= eleminationLimit){
+        _primeEliminationStage(list);
+        list = list->next;
+    }
+}
 
 
 
+int main(){
+    struct Number* buffer = makeNumberBuffer();
+    eleminateNonPrimes(buffer);
+    printNumbers(buffer);
+
+    
 }

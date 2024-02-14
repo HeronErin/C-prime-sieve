779ebae1225bbe488d4b9eccc78dad095b729324
Mitchell <80652149+HeronErin@users.noreply.github.com>
2024-02-14 17:18:26 +0000

    Add all tests

--- readmeGen/generator.py
+++ readmeGen/generator.py
@@ -28,15 +28,17 @@ results = [
         runTest(500_000, "-O2"),
         runTest(500_000, "-O3"),
 
-        # runTest(1_000_000, "-O0"),
-        # runTest(1_000_000, "-O1"),
-        # runTest(1_000_000, "-O2"),
-        # runTest(1_000_000, "-O3"),
+        runTest(1_000_000, "-O0"),
+        runTest(1_000_000, "-O1"),
+        runTest(1_000_000, "-O2"),
+        runTest(1_000_000, "-O3"),
 
-        # runTest(5_000_000, "-O0"),
-        # runTest(5_000_000, "-O1"),
-        # runTest(5_000_000, "-O2"),
-        # runTest(5_000_000, "-O3"),
+        runTest(5_000_000, "-O0"),
+        runTest(5_000_000, "-O1"),
+        runTest(5_000_000, "-O2"),
+        runTest(5_000_000, "-O3"),
+        
+        runTest(7_000_000, "-O4"),
         ]
 
 with open(os.path.dirname(__file__)+"/../historicData.csv", "a") as output:
